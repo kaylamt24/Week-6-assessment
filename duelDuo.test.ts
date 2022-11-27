e665fa47716a78559db3facd7b51b966738c7e60
Kayla Mountain <kaylamt24@gmail.com>
2022-11-26 18:26:21 -0600

    another

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -6,7 +6,7 @@ require('chromedriver')
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
 
 beforeEach(async () => {
-    driver.get('http://ec2-52-25-241-235.us-west-2.compute.amazonaws.com/')
+    driver.get('http://localhost:4000/')
 })
 
 afterAll(async () => {
